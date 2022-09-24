668509d25f421a7c6516adbd863bab4424ceb7fc
Atilola Oluwatobiloba <tobiatilola@gmail.com>
2022-09-24 05:55:21 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post request succesful\n")
+	Name := r.FormValue("name")
+	Address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %s\n", Name)
+	fmt.Fprintf(w, "Address: %s\n", Address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 0420\n")
+	if err := http.ListenAndServe(":0420", nil); err != nil {
+		log.Fatal(err)
+	}
+}
